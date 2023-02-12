a46965238b3418bd3df9bc34c5b8a75e9d141992
Juan <juan.nilo@alumnos.ucn.cl>
2023-02-11 22:36:32 -0300

    centrado

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,8 +1,13 @@
 export default function Index() {
   return (
-    <div className="title">
-      <h1>Hi i'm Juan</h1>
-      <p>developer</p>
+    <div className="container">
+      <div className="title">
+        <h1>Hi i'm Juan</h1>
+        <p>developer</p>
+      </div>
+      <div>
+        <p>Estudiante de ingenieria...</p>
+      </div>
     </div>
   );
 }
